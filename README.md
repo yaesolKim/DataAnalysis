4c2da1b3b718d22c361b1617653610fe2b099592
yaesol <yaesol@ysmacbookm1air.local>
2024-03-25 16:25:42 +0100

    upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# FFH Data
+
+plots~
---
+++ plot2.R
@@ -0,0 +1,20 @@
+# Load necessary libraries
+library(ggplot2)
+library(readxl)
+library(RColorBrewer)
+
+
+# Read data from Excel file
+#excel_data <- read_excel("execution_time.xlsx", sheet = "ex2")
+#excel_data <- read_excel("execution_time.xlsx", sheet = "ex3")
+#excel_data <- read_excel("execution_time.xlsx", sheet = "ex2_ex3")
+excel_data <- read_excel("execution_time.xlsx", sheet = "ex2_ex3")
+
+p <- ggplot(excel_data, aes(x = factor(condition), y = answer, fill = factor(condition))) +
+  geom_boxplot() +
+  #labs(x = "Conditions", y = "Answer", title = "Mental Demand: How mentally demanding was the task?") +
+  labs(x = "Conditions", y = "Execution Time (sec)", title = "") +
+  #scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9", "#56B4E9")) +
+  scale_fill_brewer(palette="Dark2")
+
+print(p)
