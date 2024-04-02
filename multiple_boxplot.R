79f67ace78a241ff10b190fdf969bce216b4768a
yaesol <yaesol@ysmacbookm1air.local>
2024-04-02 19:01:29 +0200

    add t_test script and multple box plot script

---
+++ multiple_boxplot.R
@@ -0,0 +1,45 @@
+# Load necessary libraries
+library(ggplot2)
+library(readxl)
+library(RColorBrewer)
+
+
+label_x = "Scenario"
+label_y = "Nasa TLX Score (7-point Likert Scale)"
+
+p <- ggplot(excel_data, aes(x = factor(condition), y = q1, fill = factor(condition))) +
+  geom_boxplot() +
+  #labs(x = "Conditions", y = "Answer", title = "Mental Demand: How mentally demanding was the task?") +
+  labs(x = label_x, y = label_y, title = "Mental Demand (the lower, the better)") +
+  scale_fill_brewer(palette="Dark2") +
+  theme_classic()+ coord_cartesian(ylim = c(0, 7))
+
+print(p)
+
+#########
+
+# Read data from Excel file
+excel_data <- read_excel("./FFH/exp2.xlsx", sheet = "nasa_tlx")
+
+variety <- factor(excel_data$question)
+treatment <- factor(excel_data$condition)
+note <- excel_data$answer
+
+data <- data.frame(variety, treatment, note)
+
+# Create the plot
+ggplot(data, aes(x = variety, y = note, fill = treatment)) +
+  geom_boxplot() +
+  labs(x = "Treatment", y = "Answers (7-point Likert Scale)", title = "Box Plot of Note by Treatment and Variety") +
+  theme_classic()
+
+#
+#c8 <- excel_data$Cut8
+#
+## t-test 수행
+#result1 <- t.test(o, o2)
+#t_test_table1 <- data.frame(
+#  Statistics = c("t", "df", "p-value", "Mean_Original", "Mean_Original2"),
+#  Values = c(sprintf("%.2f", result1$statistic), sprintf("%.2f",result1$parameter), sprintf("%.2f", result1$p.value), sprintf("%.2f", mean(o, na.rm = TRUE)), sprintf("%.2f", mean(o2, na.rm = TRUE)))
+#)
+#print(t_test_table1)
--- t_test.R
+++ t_test.R
@@ -3,26 +3,60 @@ library(ggplot2)
 library(readxl)
 library(RColorBrewer)
 
-#excel_data <- read_excel("./FFH/exp1.xlsx", sheet = "fps")
-excel_data <- read_excel("./FFH/exp1.xlsx", sheet = "ftime")
+# select data sheet
+#excel_data <- read_excel("./FFH/exp1.xlsx", sheet = "fps_1")
+#excel_data <- read_excel("./FFH/exp1.xlsx", sheet = "ftime_1")
+#excel_data <- read_excel("./FFH/exp1.xlsx", sheet = "fps_2")
+#excel_data <- read_excel("./FFH/exp1.xlsx", sheet = "ftime_2")
 
-original <- excel_data$Original
-original2 <- excel_data$Original2
-original4 <- excel_data$Original4
-original8 <- excel_data$Original8
-cut2 <- excel_data$Cut2
-cut4 <- excel_data$Cut4
-cut8 <- excel_data$Cut8
+o <- excel_data$Original
+o2 <- excel_data$Original2
+o4 <- excel_data$Original4
+o8 <- excel_data$Original8
+c2 <- excel_data$Cut2
+c4 <- excel_data$Cut4
+c8 <- excel_data$Cut8
 
 # t-test 수행
-result <- t.test(original, cut8)
-print(result)
-
-# 요약된 결과를 데이터 프레임으로 변환
-t_test_table <- data.frame(
-  Statistics = c("t", "df", "p-value", "Mean of Group A", "Mean of Group B"),
-  Values = c(result$statistic, result$parameter, result$p.value, mean(original,na.rm = T), mean(cut8,na.rm = T))
+result1 <- t.test(o, o2)
+t_test_table1 <- data.frame(
+  Statistics = c("t", "df", "p-value", "Mean_Original", "Mean_Original2"),
+  Values = c(sprintf("%.2f", result1$statistic), sprintf("%.2f",result1$parameter), sprintf("%.2f", result1$p.value), sprintf("%.2f", mean(o, na.rm = TRUE)), sprintf("%.2f", mean(o2, na.rm = TRUE)))
 )
+print(t_test_table1)
 
-# 테이블 출력
-print(t_test_table)
+result2 <- t.test(o, o4)
+t_test_table2 <- data.frame(
+  Statistics = c("t", "df", "p-value", "Mean_Original", "Mean_Original2"),
+  Values = c(sprintf("%.2f", result2$statistic), sprintf("%.2f",result2$parameter), sprintf("%.2f", result2$p.value), sprintf("%.2f", mean(o, na.rm = TRUE)), sprintf("%.2f", mean(o4, na.rm = TRUE)))
+)
+print(t_test_table2)
+
+result3 <- t.test(o, o8)
+t_test_table3 <- data.frame(
+  Statistics = c("t", "df", "p-value", "Mean_Original", "Mean_Original2"),
+  Values = c(sprintf("%.2f", result3$statistic), sprintf("%.2f",result3$parameter), sprintf("%.2f", result3$p.value), sprintf("%.2f", mean(o, na.rm = TRUE)), sprintf("%.2f", mean(o8, na.rm = TRUE)))
+)
+print(t_test_table3)
+
+result4 <- t.test(o, c2)
+t_test_table4 <- data.frame(
+  Statistics = c("t", "df", "p-value", "Mean_Original", "Mean_Original2"),
+  Values = c(sprintf("%.2f", result4$statistic), sprintf("%.2f",result4$parameter), sprintf("%.2f", result4$p.value), sprintf("%.2f", mean(o, na.rm = TRUE)), sprintf("%.2f", mean(c2, na.rm = TRUE)))
+)
+print(t_test_table4)
+
+result5 <- t.test(o, c4)
+t_test_table5 <- data.frame(
+  Statistics = c("t", "df", "p-value", "Mean_Original", "Mean_Original2"),
+  Values = c(sprintf("%.2f", result5$statistic), sprintf("%.2f",result5$parameter), sprintf("%.2f", result5$p.value), sprintf("%.2f", mean(o, na.rm = TRUE)), sprintf("%.2f", mean(c4, na.rm = TRUE)))
+)
+print(t_test_table5)
+
+result6 <- t.test(o, c8)
+t_test_table6 <- data.frame(
+  Statistics = c("t", "df", "p-value", "Mean_Original", "Mean_Original2"),
+  Values = c(sprintf("%.2f", result6$statistic), sprintf("%.2f",result6$parameter), sprintf("%.2f", result6$p.value), sprintf("%.2f", mean(o, na.rm = TRUE)), sprintf("%.2f", mean(c8, na.rm = TRUE)))
+)
+print(t_test_table6)
+
